d526d27357386d1708d1b576108f8c7e5e3e7d2c
Kamil <kamilcz2001@gmail.com>
2024-11-23 22:25:25 +0100

    feat: add Hero component and update styles for headers

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,23 @@
 import "./Hero.scss";
 import React from "react";
+import {Button} from "../Button/Button.tsx";
 
 export const Hero : React.FC = () => {
     return (
         <section className="hero overlay">
-
+            <div className="hero__content">
+                <h1 className="header--primary">
+                    Challenge your limits and
+                    achieve your fitness goals
+                </h1>
+                <p className="paragraph">
+                    Push beyond your comfort zone and unlock your true potential, commit to the process and celebrate
+                    every small victory along the way.
+                </p>
+                <Button type="button">
+                    Create Free Account
+                </Button>
+            </div>
         </section>
     );
 };
