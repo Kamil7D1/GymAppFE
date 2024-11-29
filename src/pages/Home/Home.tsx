c44a939cf9e86a039c46e1a06a760547a8ed2912
Kamil Czudaj <kamilcz2001@gmail.com>
2024-11-29 22:11:59 +0100

    feat: add Coach component and update styles in BenefitCard

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import "./Home.scss";
 import React from "react";
 import {Hero} from "../../components/Hero/Hero.tsx";
 import {Benefit} from "../../components/Benefit/Benefit.tsx";
+import {Coach} from "../../components/Coach/Coach.tsx";
 import {About} from "../../components/About/About.tsx";
 
 export const Home : React.FC = () => {
@@ -9,6 +10,7 @@ export const Home : React.FC = () => {
         <div className="home">
             <Hero/>
             <Benefit/>
+            <Coach/>
             <About/>
         </div>
     );
